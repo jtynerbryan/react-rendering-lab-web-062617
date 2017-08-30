5dc16a8b05571b735ad45d2dc80bb56bd9d8c324
Jesse Tyner-Bryan <jesse@Jesses-MacBook-Air.local>
2017-08-30 16:01:03 -0400

    Done.

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -11,6 +11,22 @@ class Rating extends React.Component {
     };
   }
 
+  componentWillReceiveProps(nextProps) {
+    debugger
+    if (nextProps.rating > this.props.rating) {
+      this.setState({
+        increasing: true,
+        decreasing: false
+      })
+    } else if (nextProps.rating < this.props.rating) {
+      this.setState({
+        increasing: false,
+        decreasing: true        
+      })
+    }
+
+  }
+
   render() {
     let trend = 'stable';
     if (this.state.increasing) {
